2da4bb1d471beec5caf7ccdc2a52cc3564564993
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-10-05 00:35:26 +0200

    succesfully tested fib

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,7 @@
 function fibonacci(num) {
+  if (num===0){
+    return 0;
+  }
   let index=2;
   let fib1=1
   let fib2=1
@@ -12,5 +15,5 @@ return fib2
 }
 
 module.exports = fibonacci;
-console.log(fibonacci(3)); // 2
+console.log(fibonacci(2)); // 2
 console.log(fibonacci(7)); // 13
